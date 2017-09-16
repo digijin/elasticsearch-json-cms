e979ed594a2e3d4083de454379db72de5d22be2b
digijin <james@digijin.com>
2017-09-16 11:42:31 +0100

    webpack babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,16 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js/,
+				loader: "babel-loader"
+			}
+		]
+	},
+	resolve: {
+		modules: ["node_modules", path.resolve(__dirname, "src", "client")]
 	}
 };
