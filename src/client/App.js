36ae37de8f85fc0cd28a9811d555d7efa7b7b45d
digijin <james@digijin.com>
2017-09-16 12:02:56 +0100

    react and material

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,53 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+import Paper from "material-ui/Paper";
+
+import {
+	Table,
+	TableBody,
+	TableHeader,
+	TableHeaderColumn,
+	TableRow,
+	TableRowColumn
+} from "material-ui/Table";
+
 export default class App {
 	constructor(container) {
-		container.innerHTML = "asd";
+		this.container = container;
+		// container.innerHTML = "asd";
+		this.render();
+	}
+	render() {
+		ReactDOM.render(
+			<div>
+				<MuiThemeProvider>
+					<Paper>
+						Here is some data
+						<Table>
+							<TableHeader>
+								<TableRow>
+									<TableHeaderColumn>ID</TableHeaderColumn>
+									<TableHeaderColumn>Name</TableHeaderColumn>
+									<TableHeaderColumn>
+										Status
+									</TableHeaderColumn>
+								</TableRow>
+							</TableHeader>
+							<TableBody>
+								<TableRow>
+									<TableRowColumn>1</TableRowColumn>
+									<TableRowColumn>John Smith</TableRowColumn>
+									<TableRowColumn>Employed</TableRowColumn>
+								</TableRow>
+							</TableBody>
+						</Table>
+					</Paper>
+				</MuiThemeProvider>
+			</div>,
+			this.container
+		);
 	}
 }
