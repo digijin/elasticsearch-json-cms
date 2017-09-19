b92b366276195b1e38b0d85f7c1da67867506b7e
digijin <james@digijin.com>
2017-09-19 13:23:05 +0100

    endpoint

--- src/endpoint/index.js
+++ src/endpoint/index.js
@@ -1,9 +1,21 @@
 const Koa = require("koa");
 const app = new Koa();
+import config from "../config";
+import client from "../dbclient.js";
 
 // response
-app.use(ctx => {
-	ctx.body = "Hello Koa";
+app.use(async (ctx, next) => {
+	let path = ctx.path.split("/");
+	path[0] = "root";
+	path = path.join(":");
+	let result = await client.get({
+		index: config.index,
+		type: "_all",
+		id: path
+	});
+	ctx.body = result;
+	next();
 });
 
-app.listen(8000);
+app.listen(7000);
+console.log("endpoint on", 7000);
